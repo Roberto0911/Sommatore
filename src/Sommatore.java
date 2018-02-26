1404c909649f8dfa43920d83f8fadf27fab1b450
Mario Vernetti <mariovernett@gmail.com>
2018-02-26 08:39:24 +0100

    aggiornamento

---
+++ src/Sommatore.java
@@ -0,0 +1,31 @@
+
+public class Sommatore 
+{
+	public static int somma4 (int x)
+	{
+		return x+4;
+	}
+	public static int somma10 (int x)
+	{
+		return x+10;
+	}
+	public static int somma20 (int x)
+	{
+		return x+20;
+	}
+	public static int somma100 (int x)
+	{
+		return x+100;
+	}
+	public static int somma1000 (int x)
+	{
+		return x+1000;
+	}
+	
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
