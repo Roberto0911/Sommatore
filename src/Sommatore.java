5faf70ab4c6255ec0784b7a3e2acda080c49713b
Roberto0911 <36838109+Roberto0911@users.noreply.github.com>
2018-02-26 09:04:32 +0100

    Aggiunto somma2000

--- src/Sommatore.java
+++ src/Sommatore.java
@@ -21,11 +21,20 @@ public class Sommatore
 	{
 		return x+1000;
 	}
-	
+	public static int somma2000 (int x)
+	{
+		return x+2000;
+	}
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
-
+		int x=1;
+		System.out.println(somma4(x));
+		System.out.println(somma10(x));
+		System.out.println(somma20(x));
+		System.out.println(somma100(x));
+		System.out.println(somma1000(x));
+		System.out.println(somma2000(x));
 	}
 
 }
